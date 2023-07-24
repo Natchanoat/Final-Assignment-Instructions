6d68dca5a3cc49ebbe9266d4cac7a6c0d930c8a6
Natchanoat <140337255+Natchanoat@users.noreply.github.com>
2023-07-24 15:54:05 +0700

    Add files via upload

---
+++ notebook.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. SQL
+# 3. Java
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. PyTorch
+# 2. TensorFlow
+# 3. scikit-learn
+
+# |Data Science Tools|
+# |-------------------|
+# |IDLE|
+# |Visual Studio|
+# |Thonny|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[7]:
+
+
+print((3*4)+5)
+#This a simple arithmetic expression to multiply then add integers
+
+
+# In[5]:
+
+
+print("200 minutes")
+print("=")
+print(str(200/60) + "  hours")
+#This will convert 200 minutes to hours by diving by 60
+
+
+# __Objectives: List popular languages for Data science__
+# 
+# - Python
+# - R
+# - Java
+
+# ## Author
+# Natchanok
+
+# In[ ]:
+
+
+
+
