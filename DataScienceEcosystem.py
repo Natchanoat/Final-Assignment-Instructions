1f4315222521d28752be336aae8bdf249eb05f49
Natchanoat <140337255+Natchanoat@users.noreply.github.com>
2023-07-25 16:22:37 +0700

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. SQL
+# 3. Java
+# 4. C++
+# 5. Scala
+# 6. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. PyTorch
+# 2. TensorFlow
+# 3. scikit-learn
+# 4. Numpy
+# 5. Pandas
+# 6. Apache Spark
+
+# |Data Science Tools|
+# |-------------------|
+# |Visual Studio|
+# |Thonny|
+# |Anaconda|
+# |R Studio|
+# |Spyder|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+#This a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# __Objectives:__
+# 
+# - Add comments to code cells
+# - Type a little bit expressions
+# - Create a Markdown cell
+# - Add orderd and unordered lists to markdown cells
+# - Using heading styles to markdowns cells
+
+# ## Author(s)
+# Natchanok Ruangon
